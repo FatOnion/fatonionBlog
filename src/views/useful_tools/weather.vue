deed844add0ba3c386615239314a2cee7ddcf6c8
FatOnion <1661673197@qq.com>
2025-09-25 11:06:07 +0800

    新增：字体文件

--- src/views/useful_tools/weather.vue
+++ src/views/useful_tools/weather.vue
@@ -9,13 +9,14 @@
       <div class="weather-container">
         <h2 class="text-2xl font-bold mb-6">天气查询</h2>
 
-        <div class="mb-6">
+        <div class="mb-6 flex flex-col gap-4 md:flex-row">
           <el-select
             v-model="selectedProvince"
             filterable
             placeholder="请选择省份"
             clearable
             class="w-full md:w-80"
+            @change="onProvinceChange"
           >
             <el-option
               v-for="province in provinces"
@@ -24,13 +25,29 @@
               :value="province.provinceName"
             />
           </el-select>
+
+          <el-select
+            v-if="selectedProvince && cities.length > 0"
+            v-model="selectedCity"
+            filterable
+            placeholder="请选择城市"
+            clearable
+            class="w-full md:w-80"
+          >
+            <el-option
+              v-for="city in cities"
+              :key="city.cityName"
+              :label="city.cityName"
+              :value="city.cityName"
+            />
+          </el-select>
         </div>
         <ElDivider></ElDivider>
         <div v-if="selectedProvince" class="result-container">
           <el-card>
             <template #header>
               <div class="card-header">
-                <span>{{ selectedProvince }}</span>
+                <span>{{ selectedProvince }} - {{ selectedCity || '全省' }}</span>
               </div>
             </template>
           </el-card>
@@ -46,6 +63,24 @@ import cityData from '@/assets/data/city.json'
 
 const provinces = ref<any[]>([])
 const selectedProvince = ref('')
+const cities = ref<any[]>([])
+const selectedCity = ref('')
+
+// 省份变化时，获取该省份下的城市列表
+const onProvinceChange = (provinceName: string) => {
+  selectedCity.value = ''
+  if (!provinceName) {
+    cities.value = []
+    return
+  }
+
+  const province = provinces.value.find(p => p.provinceName === provinceName)
+  if (province && province.city) {
+    cities.value = province.city
+  } else {
+    cities.value = []
+  }
+}
 
 onMounted(() => {
   provinces.value = cityData.provinces
